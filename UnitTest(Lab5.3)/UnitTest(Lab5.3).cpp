ca37e5fe422711b5d1c0ab5300b526d4219f2539
Andrew Pastushuk <andreypastushuk@gmail.com>
2020-11-07 16:12:09 +0200

    testbranch version 1.0

---
+++ UnitTest(Lab5.3)/UnitTest(Lab5.3).cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab5.3/Lab5.3.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLab53
+{
+	TEST_CLASS(UnitTestLab53)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			t = fact(4);
+			Assert::AreEqual(t, 24.00000);
+		}
+	};
+}
